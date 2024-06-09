68329635ceedcdc29695d072b05dee324aee5192
Ranjan Yadav <113471389+Ranjan095@users.noreply.github.com>
2024-06-09 10:43:08 +0530

    login form has been creatd

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-
+import React from "react";
+import logo from "../../utils/images/logo.png";
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="w-[200px] absolute left-[170px] top-2 bg-gradient-to-b rounded-lg z-10">
+      <img src={logo} alt="logo" />
+    </div>
+  );
+};
 
-export default Header
+export default Header;
--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,15 +1,39 @@
-import React from 'react'
-import Header from '../header/Header'
+import React from "react";
+import Header from "../header/Header";
+import backgroundLoginImg from "../../utils/images/backgroundLoginImg.jpg";
 
 const Login = () => {
   return (
     <div>
-        <Header/>
-        <div>
-            <h1>Login</h1>
-        </div>
+      <Header />
+      <div className="absolute ">
+        <img src={backgroundLoginImg} alt="backgroundImage" />
+      </div>
+      <div className="absolute bg-black opacity-80 w-4/12 m-auto left-0 right-0 top-[100px] px-10 py-4 rounded-md">
+        <h1 className="text-white font-bold text-3xl my-[20px]">SignIn</h1>
+        <form>
+          <input
+            type="text"
+            placeholder="Email or mobile nuber"
+            className="text-white bg-gray-800 my-2 w-full p-3 rounded-md"
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="password"
+            className="text-white bg-gray-800 my-2 w-full p-3 rounded-md"
+          />
+          <br />
+          <button
+            type="submit"
+            className="text-white font-semibold bg-[#E50914] my-2 p-2 w-full rounded-md"
+          >
+            Sign In
+          </button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
